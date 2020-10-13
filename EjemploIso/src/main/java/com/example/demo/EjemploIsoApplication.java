c58f5de6f7b733c6f634a18732628fab05bb8e03
AzelgaLetal18 <72132481+AzelgaLetal18@users.noreply.github.com>
2020-10-13 21:02:39 +0200

    Cambio

---
+++ EjemploIso/src/main/java/com/example/demo/EjemploIsoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploIsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploIsoApplication.class, args);
+	}
+
+}
